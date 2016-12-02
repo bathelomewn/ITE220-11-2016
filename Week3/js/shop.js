d8915bef4bdf5133e20ec362e45ebc93306e6abb
HP\USER <bathelomew.nwazodo@students.stamford.edu>
2016-12-02 10:38:13 +0700

    Javascript, html and css

---
+++ Week3/js/shop.js
@@ -0,0 +1,7 @@
+var customerName ="Bathe";
+var price =10;
+var quantity=2;
+var totalPrice= price*quantity;
+
+var customerElement = document.getElementByid("customer-name");
+customerElement.textContent =customerName;
