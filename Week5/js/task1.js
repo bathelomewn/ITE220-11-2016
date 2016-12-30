eecefccd063c1dfbd8600d3adc00c8b8aefb7c0c
HP\USER <bathelomew.nwazodo@students.stamford.edu>
2016-12-30 21:26:51 +0700

    My Week5- Built in Object and Event

---
+++ Week5/js/task1.js
@@ -0,0 +1,31 @@
+(function(){// on load event
+
+var task1 = {
+	render: function(){
+		document.getElementById("screenX").textContent = window.screenX;
+		document.getElementById("screenY").textContent = window.screenY;
+
+
+		document.getElementById("screenWidth").textContent = window.innerWidth;
+		document.getElementById("screenHeight").textContent = window.innerHeight;
+
+		document.getElementById("location").textContent = window.location.pathname;
+	},
+	resize: function (){
+		window.addEventListener( "resize", this.render);
+	},
+	devInfo:function(){
+		var btnDev=document.getElementById("dev-info");
+		btnDev.addEventListener("click", function(){
+			//add your code to display developer information in 'p' block
+
+		});
+	}
+		
+		
+	}
+
+task1.render();
+task1.resize();
+task1.devInfo();
+})(); // end on load event
